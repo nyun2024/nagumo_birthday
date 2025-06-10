684cb3651373b805cbe3f6dec4716bfcacaed650
RByunsu <yss@rightbrain.co.kr>
2025-06-10 21:22:55 +0900

    hyvor로 변경 테스트트

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,17 +1,19 @@
-import { ReactCusdis } from "react-cusdis";
-import styles from "./Comment.module.scss";
+// import { ReactCusdis } from "react-cusdis";
+// import styles from "./Comment.module.scss";
+import { Comments } from "@hyvor/hyvor-talk-react";
 
 const Comment = () => {
   return (
-    <ReactCusdis
-      attrs={{
-        host: "https://cusdis.com",
-        appId: "c379c40f-a933-4309-9479-7c7eec724451",
-        pageId: "home",
-        pageTitle: "나구모 생일",
-        pageUrl: window.location.href,
-      }}
-    />
+    <Comments website-id={13439} page-id={1} />
+    // <ReactCusdis
+    //   attrs={{
+    //     host: "https://cusdis.com",
+    //     appId: "c379c40f-a933-4309-9479-7c7eec724451",
+    //     pageId: "home",
+    //     pageTitle: "나구모 생일",
+    //     pageUrl: window.location.href,
+    //   }}
+    // />
   );
 };
 
