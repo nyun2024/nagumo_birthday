3460ec7916fa76cfa377c7694278e598eaaf9215
nyun2024 <kws043945@gmail.com>
2025-06-10 15:28:05 +0900

    disqus로 원복

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import { DiscussionEmbed } from "disqus-react";
 
-const Comment = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://cdn.commento.io/js/commento.js";
-    script.defer = true;
-    document.body.appendChild(script);
-  }, []);
+const Comment = ({ id, title, content }) => {
+  const disqusShortname = "nagumo-birthday"; // ex: myblog
+  const disqusConfig = {
+    url: `https://nyun2024.github.io/nagumo_birthday/${id}`, // 현재 페이지의 전체 URL
+    identifier: id.toString(), // 고유한 식별자 (보통 포스트 ID)
+    title: title,
+  };
 
-  return <div id="commento"></div>;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{content}</p>
+
+      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+    </div>
+  );
 };
 
 export default Comment;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,14 @@
 import { createHashRouter } from "react-router-dom";
 import Home from "@pages/home/Home";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
+const router = createHashRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ],
+  { basename: "/" }
+);
 
 export default router;
