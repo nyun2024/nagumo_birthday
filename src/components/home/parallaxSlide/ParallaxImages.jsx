7a79fd878330a92dcc919d0629a660621ba9925c
nyun2024 <kws043945@gmail.com>
2025-06-23 16:00:43 +0900

    스타일 수정 및 정리

--- src/components/home/parallaxSlide/ParallaxImages.jsx
+++ src/components/home/parallaxSlide/ParallaxImages.jsx
@@ -25,17 +25,20 @@ export const SlideImages = {
   sec01: {
     big: [sec01_big],
     small: [sec01_1, sec01_2, sec01_3, sec01_4, sec01_5],
-    text: "僕優しいからさ～\n特別に死に方選ば\nせてあげるよ",
+    text: "僕優しいからさ\u007E\n特別に死に方選ば\nせてあげるよ",
+    pcText: "僕優しいからさ\u007E\n特別に死に方選ばせてあげるよ",
   },
   sec02: {
     big: [sec02_big],
     small: [sec02_1, sec02_2, sec02_3, sec02_4, sec02_5],
-    text: "大丈夫ー殺しの免許は\nゴールドだから～",
+    text: "大丈夫ー殺しの免許は\nゴールドだから\u007E",
+    pcText: "大丈夫ー殺しの免許は\nゴールドだから\u007E",
   },
   sec03: {
     big: [sec03_big],
     small: [sec03_1, sec03_2, sec03_3, sec03_4, sec03_5, sec03_6],
     text: "殺しに必要なのは\n嘘でしょ",
+    pcText: "殺しに必要なのは\n嘘でしょ",
   },
 };
 
--- src/components/home/parallaxSlide/ParallaxSlide.jsx
+++ src/components/home/parallaxSlide/ParallaxSlide.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import { SlideImages } from "./ParallaxImages";
 import classNames from "classnames";
-import styles from './ParallaxSlide.module.scss'
+import styles from "./ParallaxSlide.module.scss";
+import useIsMobile from "@utils/useIsMobile";
 
 const ParallaxSlide = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -10,10 +11,13 @@ const ParallaxSlide = () => {
   const slideKeys = Object.keys(SlideImages);
   const totalSlides = slideKeys.length;
   const keys = Object.keys(SlideImages);
+  const isMobile = useIsMobile();
 
   // 슬라이드 넘어갈때 text 타이핑 효과
-    useEffect(() => {
-    const fullText = SlideImages[slideKeys[currentIndex]].text;
+  useEffect(() => {
+    const fullText = isMobile
+      ? SlideImages[slideKeys[currentIndex]].text
+      : SlideImages[slideKeys[currentIndex]].pcText;
     setTypedText("");
 
     const intervalRef = { id: null };
@@ -48,7 +52,8 @@ const ParallaxSlide = () => {
   // smallImg 패럴럭스 효과
   useEffect(() => {
     const handleScroll = () => {
-      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
 
       Object.entries(smallImgRefs.current).forEach(([, el]) => {
         if (!el) return;
@@ -72,59 +77,73 @@ const ParallaxSlide = () => {
   }, []);
 
   return (
-    <div  className={styles.parallaxSlideWrapper}>
-      <div className={styles.parallaxSlideInner}> 
-        {Object.entries(SlideImages).map(([key, item], index) =>
-          <div
+    <div
+      className={classNames(
+        styles.parallaxSlideWrapper,
+        !isMobile && styles.pc
+      )}
+    >
+      {Object.entries(SlideImages).map(([key, item], index) => (
+        <div
+          className={classNames(
+            styles.slideItem,
+            styles[`${key}SlideItem`],
+            index === currentIndex && styles.activeSlide
+          )}
+          key={`slide-${index}`}
+        >
+          <img
+            key={`big-${key}-${index}`}
+            src={item.big}
             className={classNames(
-              styles.slideItem,
-              styles[`${key}SlideItem`],
-              index === currentIndex && styles.activeSlide
+              styles.bigImg,
+              styles[`${key}BigImg`],
+              index === currentIndex && styles.fadeIn
             )}
-            key={`slide-${index}`}
-          >
-            <img
-              key={`big-${key}-${index}`}
-              src={item.big}
-              className={classNames(
-                styles.bigImg,
-                styles[`${key}BigImg`],
-                index === currentIndex && styles.fadeIn
-              )}
-            />
-            {item.small.map((smallItem, i) => (
-              <div
-                className={classNames(styles.smallImg, styles[`small0${i + 1}`])}
-                key={`small-${i}`}
-                ref={(el) => {
-                  if (el) smallImgRefs.current[`${key}-${i}`] = el;
-                }}
-                data-speed={(i + 1) * 0.05}
+          />
+          {item.small.map((smallItem, i) => (
+            <div
+              className={classNames(styles.smallImg, styles[`small0${i + 1}`])}
+              key={`small-${i}`}
+              ref={(el) => {
+                if (el) smallImgRefs.current[`${key}-${i}`] = el;
+              }}
+              data-speed={(i + 1) * 0.05}
+            >
+              <img src={smallItem} alt="small img" />
+            </div>
+          ))}
+          <p className={styles.typingText}>
+            {typedText.split("").map((char, i) => (
+              <span
+                key={i}
+                className={styles.typingChar}
+                style={{ animationDelay: `${i * 0.03}s` }}
               >
-                <img src={smallItem} alt="small img" />
-              </div>
+                {char}
+              </span>
             ))}
-            <p className={styles.typingText}>
-              {typedText.split("").map((char, i) => (
-                <span key={i} className={styles.typingChar} style={{ animationDelay: `${i * 0.03}s` }}>
-                  {char}
-                </span>
-              ))}
-            </p>
-          </div>
-        )}
-        <div className={classNames(styles.slidePagination, currentIndex === 1 && styles.sec02Page)}>
-          <button type="button" className={styles.prevBtn} onClick={handlePrev}>
-            <span className="blind">prev button</span>
-          </button>
-          <div className={styles.pageNum}>{currentIndex + 1} / {totalSlides}</div>
-          <button type="button" className={styles.nextBtn} onClick={handleNext}>
-            <span className="blind">next button</span>
-          </button>
+          </p>
         </div>
+      ))}
+      <div
+        className={classNames(
+          styles.slidePagination,
+          currentIndex === 1 && styles.sec02Page
+        )}
+      >
+        <button type="button" className={styles.prevBtn} onClick={handlePrev}>
+          <span className="blind">prev button</span>
+        </button>
+        <div className={styles.pageNum}>
+          {currentIndex + 1} / {totalSlides}
+        </div>
+        <button type="button" className={styles.nextBtn} onClick={handleNext}>
+          <span className="blind">next button</span>
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ParallaxSlide
+export default ParallaxSlide;
